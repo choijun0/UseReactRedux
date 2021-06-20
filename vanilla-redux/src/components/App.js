a407c59fe92de8a4c915539aa7cc186b2510ef07
choi2 <>
2021-06-20 11:23:28 +0000

    <DeleteToDo & Routing + Params obj with useParams(react-hook)>
    React function component 에 useParams()를 실행하면 ||해당 컴포너트|| 동적 라우팅 변수의 이름과 값을 가지는 객체를 반환한다. example<Route path=/:id component={Detail}></Route> //id is dynamic route variable useParams() === { id : dynamicValue }

--- vanilla-redux/src/components/App.js
+++ vanilla-redux/src/components/App.js
@@ -3,7 +3,7 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import Home from "../routes/Home";
 import Detail from "../routes/Detail";
 
-function App(){
+function App(){ 
 	return (
 	  <Router>
 	    <Route path="/" exact component={Home}></Route>
--- vanilla-redux/src/routes/Detail.js
+++ vanilla-redux/src/routes/Detail.js
@@ -1,1 +1,22 @@
-export default () => "Detail";
+import React from "react";
+import {connect} from "react-redux";
+import {useParams} from "react-router-dom";
+
+const Detail = ( {todo} ) => {
+	return (
+		<>
+		  <span>{todo?.text}</span>
+		  <h5>Created at: {todo?.id}</h5>
+		</>
+	)
+}
+//Array.find returns what pass test function
+//params obj is equal object what useParams (react Hook) returning.
+const mapStateToProps= (state, ownProps) => {
+	const {match : {params : {id}}} = ownProps;
+	console.log(ownProps);
+	return {
+		todo : state.find(toDo => toDo.id === parseInt(id))
+	}
+}
+export default connect(mapStateToProps)(Detail);
