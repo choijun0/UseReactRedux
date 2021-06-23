01459ba5ec7d5e4fbbfb4faf29eea46f8e7428b2
choi2 <>
2021-06-23 13:44:50 +0000

    <ReduxToolKit/createReducer>
    explanation is in line 9-36 in store.js

--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,11 +1,12 @@
 import {createStore} from "redux";
 import { writeFile } from "./text/fileInOut";
-import {createAction} from "@reduxjs/toolkit";
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
 //Const
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
 
+/*Basic Redux Reducer
 const reducer = (state=[], action) => {
 	switch(action.type){
 		case addTodo.type:
@@ -18,6 +19,21 @@ const reducer = (state=[], action) => {
 		  return state;
 	}
 }
+*/
+
+//CreateReducer
+//!!mutate state (don't return) or return new state!!
+//if I mutate state createReducer out of ReduxToolkit transfrom it to return new state automatically
+const reducer = createReducer([], {
+	[addTodo] : (state, action) => {
+		state.push({text:action.payload, id : Date.now()});
+	},
+	[deleteTodo] : (state, action) => {
+		return state.filter(toDo => 
+				toDo.id !== action.payload
+			);
+	}
+});
 
 const store = createStore(reducer);
 
